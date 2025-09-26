7cf94891190bc777f756f8e31850ccad5b1b740d
maicon.clemer <maicon.clemer@gmail.com>
2025-09-26 20:50:43 -0300

    Adicionar arquivos de projeto.

---
+++ JovemProgramadorWeb/Data/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using JovemProgramadorWeb.Data.Repositorio.Interfaces;
+using JovemProgramadorWeb.Models;
+
+namespace JovemProgramadorWeb.Data.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+
+        private readonly BancoContexto _bancoContexto;
+
+        public UsuarioRepositorio(BancoContexto bancoContexto)
+        {
+            _bancoContexto = bancoContexto;
+        }
+
+        public void CadastrarUsuario(Usuario usuario)
+        {
+            _bancoContexto.Usuario.Add(usuario);
+            _bancoContexto.SaveChanges();
+        }
+
+        public Usuario ValidarUsuario(Usuario usuario)
+        {
+            return _bancoContexto.Usuario.FirstOrDefault(x => x.Email == usuario.Email && x.Senha == usuario.Senha);
+        }
+
+    }
+}
---
+++ JovemProgramadorWeb/Program.cs
@@ -0,0 +1,37 @@
+using JovemProgramadorWeb.Data.Repositorio.Interfaces;
+using JovemProgramadorWeb.Data.Repositorio;
+using JovemProgramadorWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionstring = builder.Configuration.GetConnectionString("StringConexao");
+builder.Services.AddDbContext<BancoContexto>(options => options.UseSqlServer(connectionstring));
+
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=Index}/{id?}");
+
+app.Run();
